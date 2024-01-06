e81bf0bd5a03804e95d72e706d3f417f982a452b
Vincent Giudicelli <vincent.giudicelli@free.fr>
2024-01-06 15:19:31 +0100

    other version of compute elo

--- server/Billard_ENSG/compute/compute_elo.php
+++ server/Billard_ENSG/compute/compute_elo.php
@@ -1,5 +1,5 @@
 <?php
-  function diffElo($eA, $eB, $n) {
+  function diffElo($eA, $eB, $n=1) {
     return $n * 10 / (1 + pow(10, ($eA-$eB)/400));
   }
   function compute_elo_v1($j1, $j2, $j3, $j4, $data) {
@@ -27,6 +27,52 @@
     $data[$j3]["delta_elo"] = -diffElo($data[$j3]["elo"], $eloV, $nD);
     if ($j4 > 0) {
       $data[$j4]["delta_elo"] = -diffElo($data[$j4]["elo"], $eloV, $nD);
+    }
+    foreach ([
+      ["p"=>$j1, "w"=>1],
+      ["p"=>$j2, "w"=>1],
+      ["p"=>$j3, "w"=>-1],
+      ["p"=>$j4, "w"=>-1]
+      ] as ["p"=>$p, "w"=>$w]) {
+      if ($p <= 0) {
+        continue;
+      }
+      if ($data[$p]["nb_games"] < 3) {
+        $data[$p]["delta_elo"] = $w*10;
+      }
+    }
+    return $data;
+  }
+
+  function compute_elo_v2($j1, $j2, $j3, $j4, $data) {
+    /*
+     * Match classement si au moins un joueur n'est pas classé. Sinon: match elo
+     *
+     * Match classement:
+     *    ∆Elo = ±10 pour les joueurs non classés
+     *    ∆Elo = 0 pour les joueurs classés  !!!! Problème dans la vue décompte des victoires
+     *
+     * Match elo:
+     *    ∆Elo calculé entre l'elo du joueur et l'elo moyen de l'équipe adverse,
+     *    avec k = 20 / n où n est le nombre de joueurs de l'équipe
+     */
+
+    $eloV = $j2 <= 0 ? $data[$j1]["elo"] : ($data[$j1]["elo"] + $data[$j2]["elo"]) / 2;
+    $eloD = $j4 <= 0 ? $data[$j3]["elo"] : ($data[$j3]["elo"] + $data[$j4]["elo"]) / 2;
+
+    $delta = diffElo($eloV, $eloD);
+    if ($j2 > 0) {
+      $data[$j1]["delta_elo"] = $delta;
+      $data[$j2]["delta_elo"] = $delta;
+    } else {
+      $data[$j1]["delta_elo"] = 2*$delta;
+    }
+
+    if ($j4 > 0) {
+      $data[$j3]["delta_elo"] = -$delta;
+      $data[$j4]["delta_elo"] = -$delta;
+    } else {
+      $data[$j3]["delta_elo"] = -2*$delta;
     }
     foreach ([
       ["p"=>$j1, "w"=>1],
